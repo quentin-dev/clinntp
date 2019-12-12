2d1b4a317bd1cb8f0c84673f2b332f64f110d2a6
Quentin Barbarat <quentin.barbarat@epita.fr>
2019-12-12 22:56:34 +0100

    Add tests to utils.test

--- test/utils.test.js
+++ test/utils.test.js
@@ -6,4 +6,16 @@ describe('#textArrayToString()', function () {
   it('Two lines no trailing newline', function () {
     expect.expect(textArrayToString(['line1', 'line2'])).to.equal('line1\nline2')
   })
+
+  it('Empty array', function () {
+    expect.expect(textArrayToString([])).to.equal('')
+  })
+
+  it('Only one empty string', function () {
+    expect.expect(textArrayToString([''])).to.equal('')
+  })
+
+  it('Only empty strings', function () {
+    expect.expect(textArrayToString(['', '', ''])).to.equal('\n\n')
+  })
 })
