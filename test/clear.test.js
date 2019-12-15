08a6e3cce1afde73790a737e0d59a2cbada66cf7
Quentin Barbarat <quentin.barbarat@epita.fr>
2019-12-15 01:00:21 +0100

    Fix lint errors in configure and clear tests

--- test/clear.test.js
+++ test/clear.test.js
@@ -5,16 +5,16 @@ import { clear } from '../src/clear'
 import { name } from '../package.json'
 
 describe('#clear()', function () {
-    this.beforeEach(function () {
-        const config = new Conf()
-        config.clear()
-    })
+  this.beforeEach(function () {
+    const config = new Conf()
+    config.clear()
+  })
 
-    it('clear() clears current config', function () {
-        const config = new Conf()
-        config.set(name, { limit: -1, port: 119, host: 'news.epita.fr', newsgroups: '*'})
-        assert.notEqual(config.get(name), undefined)
-        clear(config)
-        assert.equal(config.get(name), undefined)
-    })
+  it('clear() clears current config', function () {
+    const config = new Conf()
+    config.set(name, { limit: -1, port: 119, host: 'news.epita.fr', newsgroups: '*' })
+    assert.notEqual(config.get(name), undefined)
+    clear(config)
+    assert.equal(config.get(name), undefined)
+  })
 })
--- test/configure.test.js
+++ test/configure.test.js
@@ -5,61 +5,60 @@ import { configure } from '../src/configure'
 import { name } from '../package.json'
 
 describe('#configure()', function () {
-    this.beforeEach(function () {
-        const config = new Conf ()
-        config.clear()
-    })
+  this.beforeEach(function () {
+    const config = new Conf()
+    config.clear()
+  })
 
-    it('configure() sets default limit', function () {
-        const config = new Conf()
-        var currentConfig = configure(config, name)
-        assert.equal(currentConfig.limit, -1)
-    })
+  it('configure() sets default limit', function () {
+    const config = new Conf()
+    var currentConfig = configure(config, name)
+    assert.equal(currentConfig.limit, -1)
+  })
 
-    it('configure() sets default port', function () {
-        const config = new Conf()
-        var currentConfig = configure(config, name)
-        assert.equal(currentConfig.port, 119)
-    })
+  it('configure() sets default port', function () {
+    const config = new Conf()
+    var currentConfig = configure(config, name)
+    assert.equal(currentConfig.port, 119)
+  })
 
-    it('configure() sets default host', function () {
-        const config = new Conf()
-        var currentConfig = configure(config, name)
-        assert.equal(currentConfig.host, 'news.epita.fr')
-    })
+  it('configure() sets default host', function () {
+    const config = new Conf()
+    var currentConfig = configure(config, name)
+    assert.equal(currentConfig.host, 'news.epita.fr')
+  })
 
-    it('configure() sets default newsgroups', function () {
-        const config = new Conf()
-        var currentConfig = configure(config, name)
-        assert.equal(currentConfig.newsgroups, '*')
-    })
+  it('configure() sets default newsgroups', function () {
+    const config = new Conf()
+    var currentConfig = configure(config, name)
+    assert.equal(currentConfig.newsgroups, '*')
+  })
 
-    it('configure() does not overwrite current limit', function () {
-        const config = new Conf()
-        config.set(name, { limit: 4, port: 119, host: 'news.epita.fr', newsgroups: '*'})
-        var currentConfig = configure(config, name)
-        assert.equal(currentConfig.limit, 4)
-    })
+  it('configure() does not overwrite current limit', function () {
+    const config = new Conf()
+    config.set(name, { limit: 4, port: 119, host: 'news.epita.fr', newsgroups: '*' })
+    var currentConfig = configure(config, name)
+    assert.equal(currentConfig.limit, 4)
+  })
 
-    it('configure() does not overwrite current port', function () {
-        const config = new Conf()
-        config.set(name, { limit: -1, port: 42, host: 'news.epita.fr', newsgroups: '*'})
-        var currentConfig = configure(config, name)
-        assert.equal(currentConfig.port, 42)
-    })
+  it('configure() does not overwrite current port', function () {
+    const config = new Conf()
+    config.set(name, { limit: -1, port: 42, host: 'news.epita.fr', newsgroups: '*' })
+    var currentConfig = configure(config, name)
+    assert.equal(currentConfig.port, 42)
+  })
 
-    it('configure() does not overwrite current host', function () {
-        const config = new Conf()
-        config.set(name, { limit: -1, port: 119, host: 'news.gnu.com', newsgroups: '*'})
-        var currentConfig = configure(config, name)
-        assert.equal(currentConfig.host, 'news.gnu.com')
-    })
+  it('configure() does not overwrite current host', function () {
+    const config = new Conf()
+    config.set(name, { limit: -1, port: 119, host: 'news.gnu.com', newsgroups: '*' })
+    var currentConfig = configure(config, name)
+    assert.equal(currentConfig.host, 'news.gnu.com')
+  })
 
-    it('configure() does not overwrite current newsgroups', function () {
-        const config = new Conf()
-        config.set(name, { limit: -1, port: 119, host: 'news.epita.fr', newsgroups: 'assistants.*'})
-        var currentConfig = configure(config, name)
-        assert.equal(currentConfig.newsgroups, 'assistants.*')
-    })
-
+  it('configure() does not overwrite current newsgroups', function () {
+    const config = new Conf()
+    config.set(name, { limit: -1, port: 119, host: 'news.epita.fr', newsgroups: 'assistants.*' })
+    var currentConfig = configure(config, name)
+    assert.equal(currentConfig.newsgroups, 'assistants.*')
+  })
 })
