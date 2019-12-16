46ba8a40ce1dadf1be2bf62da91d50d631263b35
Quentin Barbarat <quentin.barbarat@epita.fr>
2019-12-16 16:49:47 +0100

    Add tests for main()

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { cli } from './cli'
 
 export async function main (argsArray) {
-  return await cli(argsArray)
+  return cli(argsArray)
 }
--- test/main.test.js
+++ test/main.test.js
@@ -4,17 +4,17 @@ import 'mocha-sinon'
 import { main } from '../src/main'
 
 describe('#main()', function () {
-    this.beforeEach(function () {
-        this.sinon.stub(console, 'log')
-    })
+  this.beforeEach(function () {
+    this.sinon.stub(console, 'log')
+  })
 
-    it('return 0 if correct args', async function () {
-        const argsArray = ['', '', '--dryrun']
-        expect(await main(argsArray)).to.equal(0)
-    })
+  it('return 0 if correct args', async function () {
+    const argsArray = ['', '', '--dryrun']
+    expect(await main(argsArray)).to.equal(0)
+  })
 
-    it('return 1 if correct args', async function () {
-        const argsArray = ['', '', '--dryran']
-        expect(await main(argsArray)).to.equal(1)
-    })
+  it('return 1 if correct args', async function () {
+    const argsArray = ['', '', '--dryran']
+    expect(await main(argsArray)).to.equal(1)
+  })
 })
